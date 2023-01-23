e46cd2f297774a19fce3c8774d9c54284dc0e910
michelmyseth <38660333+michelmyseth@users.noreply.github.com>
2023-01-23 13:57:58 +0100

    e2ee

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '1tjgxx',
+    projectId: "1tjgxx",
     e2e: {
         experimentalRunAllSpecs: true,
         setupNodeEvents(on, config) {
